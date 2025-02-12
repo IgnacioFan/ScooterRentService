1ad29e092c90e6880e145d9a10ed5044693d895b
Weilong <fan01856472@gmail.com>
2025-02-13 02:32:21 +0800

    access rabbitmq env variable

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18-alpine
+FROM node:20.18-alpine AS base
 
 WORKDIR /usr/src/app
 
@@ -7,6 +7,8 @@ COPY package*.json ./
 RUN npm install
 
 COPY . .
+
+RUN npx prisma generate
 
 RUN npm run build
 
--- src/rent/rent.module.ts
+++ src/rent/rent.module.ts
@@ -1,24 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RentController } from './rent.controller';
 import { RentService } from './rent.service';
 import { ScooterService } from '../scooter/scooter.service';
 import { UserService } from '../user/user.service';
-
 import { PrismaService } from '../prisma.service';
-// register microservice module (RabbitMQ)
+// Register microservice module (RabbitMQ)
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ORDER_SERVICE } from '../constant/services';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ConfigModule,
+    ClientsModule.registerAsync([
       {
         name: ORDER_SERVICE,
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://rabbitmq:5672'],
-          queue: 'order_queue',
-        },
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL') || ''],
+            queue: 'order_queue',
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
   ],
